ce32e2b022032076fcce5fb398979df108647d0a
Koryakin Vladislav <gudvin596@gmail.com>
2025-06-21 19:38:44 +0300

    hw 1 done

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class SearchTests {
+
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+}
